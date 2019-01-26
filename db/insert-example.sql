361601a30257fc9f9150230690a86235bb225fdf
Ilya Gulkov <x@isgulkov.ru>
2019-01-26 05:06:28 +0300

    Move the schema directory up to the root

--- db/insert-example.sql
+++
@@ -1,6 +0,0 @@
-insert into Orders (email, target_url, regions_and_nodes, duration)
-values ('titantins@gmail.com', 'https://order-ddos.com/', '{
-"na": 12,
-"eu": 10,
-"as": 9
-}', interval '3 hours 20 minutes');
