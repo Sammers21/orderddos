d821611adb7e995608f909152234ddd4d3f36698
Sammers21 <titantins@gmail.com>
2019-02-03 19:16:07 +0300

    polishing

--- api-server/server/src/main/java/com/orderddos/server/DDoSApiService.java
+++ api-server/server/src/main/java/com/orderddos/server/DDoSApiService.java
@@ -4,24 +4,14 @@ import io.vertx.core.Vertx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Objects;
 
 public class DDoSApiService {
 
     private static Logger log = LoggerFactory.getLogger(DDoSApiService.class);
-
     public static String OTABEK_API_KEY = "1d0866bdd5ac3a73d043769679eb71e4b788ab28129dd4d22193bd05cd0ccc40";
-
     public static final String SETUP_ENV_SCRIPT;
-
     public static final Vertx VERTX;
-
-    public static final String BASH_INTERPRETER_PATH = "/bin/bash";
-
 
     static {
         VERTX = Vertx.vertx();
@@ -30,19 +20,11 @@ public class DDoSApiService {
     }
 
     public static void main(String[] args) throws URISyntaxException {
-        final URI uri = Objects.requireNonNull(
-                Thread.currentThread().getContextClassLoader().getResource("build_virus.sh" )
-        ).toURI();
-        System.out.println(uri);
-
-        final ArrayList<String> processArgs = new ArrayList<>();
-        processArgs.add(BASH_INTERPRETER_PATH);
-        final ProcessBuilder processBuilder = new ProcessBuilder(processArgs);
-        processBuilder.directory(new File("/home/sammers/orderddos/api-server"));
-
         VERTX.deployVerticle(new ApiService(), event -> {
             if (event.succeeded()) {
                 log.info("ApiService is on");
+            } else {
+                log.error("Unable to start ApiService verticle");
             }
         });
     }
