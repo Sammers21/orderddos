ad83b90b85319aaa6ade28d513a101b53e6d61bd
Ilya Gulkov <x@isgulkov.ru>
2019-02-20 03:19:13 +0300

    Animate the number input fields (node counts & duration)

--- web-front/src/js/index.jsx
+++ web-front/src/js/index.jsx
@@ -2,6 +2,112 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+class NodeCountFields extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            na: 10,
+            eu: 0,
+            a: 0
+        }
+    }
+
+    handleNumUpdate(key, e) {
+        const newValue = parseInt(e.target.value !== "" ? e.target.value : 0);
+
+        if(isNaN(newValue)) {
+            return false;
+        }
+
+        this.setState({
+            [key]: newValue
+        });
+
+        if(this.onValueUpdate) {
+            this.onValueUpdate();
+        }
+    }
+
+    render() {
+        return (
+            <div className="form-group">
+                <label>Node count</label>
+                <div className="row">
+                    <div className="col">
+                        <div className="input-group input-group-sm">
+                            <div className="input-group-prepend">
+                                <label className="input-group-text" htmlFor="num_nodes_na">North America</label>
+                            </div>
+                            <input className="form-control form-control-sm" type="text" name="num_nodes_na"
+                                   value={this.state.na} onChange={e => this.handleNumUpdate('na', e)} />
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="input-group input-group-sm">
+                            <div className="input-group-prepend">
+                                <label className="input-group-text" htmlFor="num_nodes_eu">Europe</label>
+                            </div>
+                            <input className="form-control form-control-sm" type="text" name="num_nodes_eu"
+                                   value={this.state.eu} onChange={e => this.handleNumUpdate('eu', e)} />
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="input-group input-group-sm">
+                            <div className="input-group-prepend">
+                                <label className="input-group-text" htmlFor="num_nodes_a">Asia</label>
+                            </div>
+                            <input className="form-control form-control-sm" type="text" name="num_nodes_a"
+                                   value={this.state.a} onChange={e => this.handleNumUpdate('a', e)} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class DurationField extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: 30
+        };
+    }
+
+    handleChange(e) {
+        const newValue = parseInt(e.target.value !== "" ? e.target.value : 0);
+
+        if(isNaN(newValue)) {
+            return false;
+        }
+
+        this.setState({
+            value: newValue
+        });
+
+        if(this.onValueUpdate) {
+            this.onValueUpdate();
+        }
+    }
+
+    render() {
+        return (
+            <div className="col form-group">
+                <label htmlFor="duration">Duration</label>
+                <div className="input-group">
+                    <input className="form-control" type="text" id="duration" name="duration"
+                           value={this.state.value} onChange={e => this.handleChange(e)} />
+                    <div className="input-group-append">
+                        <div className="input-group-text">minutes</div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
 class OrderForm extends React.Component {
     render() {
         return (
@@ -35,35 +141,7 @@ class OrderForm extends React.Component {
                             </div>
                         </div>
 
-                        <div className="form-group">
-                            <label>Node count</label>
-                            <div className="row">
-                                <div className="col">
-                                    <div className="input-group input-group-sm">
-                                        <div className="input-group-prepend">
-                                            <label className="input-group-text" htmlFor="num_nodes_na">North America</label>
-                                        </div>
-                                        <input className="form-control form-control-sm" type="text" id="num_nodes_na" name="num_nodes_na" placeholder="1488" value="10" />
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="input-group input-group-sm">
-                                        <div className="input-group-prepend">
-                                            <label className="input-group-text" htmlFor="num_nodes_eu">Europe</label>
-                                        </div>
-                                        <input className="form-control form-control-sm" type="text" id="num_nodes_eu" name="num_nodes_eu" placeholder="1488" />
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="input-group input-group-sm">
-                                        <div className="input-group-prepend">
-                                            <label className="input-group-text" htmlFor="num_nodes_a">Asia</label>
-                                        </div>
-                                        <input className="form-control form-control-sm" type="text" id="num_nodes_a" name="num_nodes_a" placeholder="1488" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <NodeCountFields/>
 
                         <div className="form-row">
                             <div className="col form-group">
@@ -75,15 +153,7 @@ class OrderForm extends React.Component {
                                     <input className="form-control" type="datetime-local" id="start_time" name="start_time" placeholder="2010-10-10 10:10:10" disabled />
                                 </div>
                             </div>
-                            <div className="col form-group">
-                                <label htmlFor="duration">Duration</label>
-                                <div className="input-group">
-                                    <input className="form-control" type="text" id="duration" name="duration" placeholder="47" value="47" />
-                                    <div className="input-group-append">
-                                        <div className="input-group-text">minutes</div>
-                                    </div>
-                                </div>
-                            </div>
+                            <DurationField/>
                         </div>
 
                         <div className="form-group form-row">
