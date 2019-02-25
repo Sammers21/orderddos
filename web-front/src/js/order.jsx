49469f4edf21cb14de3ebc8de25f137c7ac38220
Ilya Gulkov <x@isgulkov.ru>
2019-02-25 07:47:42 +0300

    Remove 1 level of excess indentation

--- web-front/src/js/order.jsx
+++ web-front/src/js/order.jsx
@@ -84,154 +84,152 @@ class OrderForm extends React.Component {
     }
 
     render() {
-        return (
-            <div className="mx-auto" style={{maxWidth: "600px"}}>
-                <p>
-                    Evaluate DDoS-vulnerability of your <s>competitors</s> servers.
-                </p>
+        return <div className="mx-auto" style={{maxWidth: "600px"}}>
+            <p>
+                Evaluate DDoS-vulnerability of your <s>competitors</s> servers.
+            </p>
 
-                <div className="card" id="form-order">
-                    <form className="card-body" action="/submit-order" method="POST" onSubmit={e => this.handleSubmit(e)}>
-                        <h3 className="text-center mb-4">Order a DDoS attack</h3>
+            <div className="card" id="form-order">
+                <form className="card-body" action="/submit-order" method="POST" onSubmit={e => this.handleSubmit(e)}>
+                    <h3 className="text-center mb-4">Order a DDoS attack</h3>
 
-                        <div className="form-group">
-                            <div className="form-row">
-                                <label className="col-2 col-form-label" htmlFor="email">E-mail</label>
-                                <div className="col-10">
-                                    <div className="input-group">
-                                        <input className={"form-control" + ((this.state.hadSubmitAttempt && !this.validateEmail()) ? " is-invalid" : "")}
-                                               type="text" id="email" name="email"
-                                               autoFocus={true} autoComplete="email"
-                                               placeholder="titantins@gmail.com"
-                                               value={this.state.email}
-                                               onChange={e => this.handleEmailUpdate(e.target.value)} />
-                                        <div className="invalid-feedback">
-                                          Please provide an E-mail address.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <small className="form-text text-muted">
-                                You'll be personally contacted by Pavel Drankov, our CEO.
-                            </small>
-                        </div>
-
-                        <hr />
-
-                        <div className="form-group form-row">
-                            <label className="col-2 col-form-label" htmlFor="target_url">URL</label>
+                    <div className="form-group">
+                        <div className="form-row">
+                            <label className="col-2 col-form-label" htmlFor="email">E-mail</label>
                             <div className="col-10">
                                 <div className="input-group">
-                                    <input className={"form-control" + ((this.state.hadSubmitAttempt && !this.validateTargetUrl()) ? " is-invalid" : "")}
-                                           type="text" id="target_url" name="target_url"
-                                           autoComplete="url"
-                                           placeholder="https://github.com/Sammers21/"
-                                           value={this.state.targetUrl}
-                                           onChange={e => this.handleTargetUrlUpdate(e.target.value)} />
+                                    <input className={"form-control" + ((this.state.hadSubmitAttempt && !this.validateEmail()) ? " is-invalid" : "")}
+                                           type="text" id="email" name="email"
+                                           autoFocus={true} autoComplete="email"
+                                           placeholder="titantins@gmail.com"
+                                           value={this.state.email}
+                                           onChange={e => this.handleEmailUpdate(e.target.value)} />
                                     <div className="invalid-feedback">
-                                      Please provide a target URL.
+                                      Please provide an E-mail address.
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <small className="form-text text-muted">
+                            You'll be personally contacted by Pavel Drankov, our CEO.
+                        </small>
+                    </div>
 
-                        <div className="form-group">
-                            <label>Node count</label>
-                            <div className="row">
-                                <div className="col">
-                                    <div className="input-group input-group-sm">
-                                        <div className="input-group-prepend">
-                                            <label className="input-group-text" htmlFor="num_nodes_na">North America</label>
-                                        </div>
-                                        <PositiveIntegerField className="form-control form-control-sm" name="num_nodes_na"
-                                                              value={this.state.numNa} onInput={v => this.handleUpdate('numNa', v)} />
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="input-group input-group-sm">
-                                        <div className="input-group-prepend">
-                                            <label className="input-group-text" htmlFor="num_nodes_eu">Europe</label>
-                                        </div>
-                                        <PositiveIntegerField className="form-control form-control-sm" name="num_nodes_eu"
-                                                               value={this.state.numEu} onInput={v => this.handleUpdate('numEu', v)} />
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="input-group input-group-sm">
-                                        <div className="input-group-prepend">
-                                            <label className="input-group-text" htmlFor="num_nodes_a">Asia</label>
-                                        </div>
-                                        <PositiveIntegerField className="form-control form-control-sm" name="num_nodes_a"
-                                                              value={this.state.numA} onInput={v => this.handleUpdate('numA', v)} />
-                                    </div>
+                    <hr />
+
+                    <div className="form-group form-row">
+                        <label className="col-2 col-form-label" htmlFor="target_url">URL</label>
+                        <div className="col-10">
+                            <div className="input-group">
+                                <input className={"form-control" + ((this.state.hadSubmitAttempt && !this.validateTargetUrl()) ? " is-invalid" : "")}
+                                       type="text" id="target_url" name="target_url"
+                                       autoComplete="url"
+                                       placeholder="https://github.com/Sammers21/"
+                                       value={this.state.targetUrl}
+                                       onChange={e => this.handleTargetUrlUpdate(e.target.value)} />
+                                <div className="invalid-feedback">
+                                  Please provide a target URL.
                                 </div>
                             </div>
                         </div>
+                    </div>
 
-                        <div className="form-row">
-                            <div className="col form-group">
-                                <label htmlFor="start_time">Start time</label>
-                                <OptDateTimeField name="start_time" onInput={newDate => this.handleUpdate('startTime', newDate)} />
-                            </div>
-
-                            <div className="col form-group">
-                                <label htmlFor="duration">Duration</label>
-                                <div className="input-group">
-                                    <PositiveIntegerField className="form-control" name="duration"
-                                                          value={this.state.duration} onInput={v => this.handleUpdate('duration', v)}/>
-                                    <div className="input-group-append">
-                                        <label className="input-group-text" htmlFor="duration">minutes</label>
+                    <div className="form-group">
+                        <label>Node count</label>
+                        <div className="row">
+                            <div className="col">
+                                <div className="input-group input-group-sm">
+                                    <div className="input-group-prepend">
+                                        <label className="input-group-text" htmlFor="num_nodes_na">North America</label>
                                     </div>
+                                    <PositiveIntegerField className="form-control form-control-sm" name="num_nodes_na"
+                                                          value={this.state.numNa} onInput={v => this.handleUpdate('numNa', v)} />
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="input-group input-group-sm">
+                                    <div className="input-group-prepend">
+                                        <label className="input-group-text" htmlFor="num_nodes_eu">Europe</label>
+                                    </div>
+                                    <PositiveIntegerField className="form-control form-control-sm" name="num_nodes_eu"
+                                                           value={this.state.numEu} onInput={v => this.handleUpdate('numEu', v)} />
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="input-group input-group-sm">
+                                    <div className="input-group-prepend">
+                                        <label className="input-group-text" htmlFor="num_nodes_a">Asia</label>
+                                    </div>
+                                    <PositiveIntegerField className="form-control form-control-sm" name="num_nodes_a"
+                                                          value={this.state.numA} onInput={v => this.handleUpdate('numA', v)} />
                                 </div>
                             </div>
                         </div>
+                    </div>
 
-                        <div className="form-group form-row">
-                            <label className="col-2 col-form-label" htmlFor="target_url">Subtotal</label>
-                            <div className="col-10 col-form-label">
-                                {
-                                    this.isInputNonzero() ? <>
-                                        <span className="text-muted">
-                                            <strong>{this.props.baseCost}</strong>
-                                            {(this.state.numNa !== 0) && <>
-                                                &nbsp;+&nbsp;
-                                                <strong>{this.state.numNa}</strong>&thinsp;&times;&thinsp;<strong>{this.state.duration}</strong>&thinsp;&times;&thinsp;<strong>{this.props.costNa}</strong>
-                                            </>}
-                                            {(this.state.numEu !== 0) && <>
-                                                &nbsp;+&nbsp;
-                                                <strong>{this.state.numEu}</strong>&thinsp;&times;&thinsp;<strong>{this.state.duration}</strong>&thinsp;&times;&thinsp;<strong>{this.props.costEu}</strong>
-                                            </>}
-                                            {(this.state.numA !== 0) && <>
-                                                &nbsp;+&nbsp;
-                                                <strong>{this.state.numA}</strong>&thinsp;&times;&thinsp;<strong>{this.state.duration}</strong>&thinsp;&times;&thinsp;<strong>{this.props.costA}</strong>
-                                            </>}
-                                            &nbsp;=&nbsp;
-                                        </span>
-                                        <strong>{
-                                            (
-                                                this.props.baseCost
-                                                    + (this.state.numNa * this.state.duration * this.props.costNa)
-                                                    + (this.state.numEu * this.state.duration * this.props.costEu)
-                                                    + (this.state.numA * this.state.duration * this.props.costA)
-                                            ).toFixed(2)
-                                        }</strong> ₽
-                                    </> : <span className="text-muted">—</span>
-                                }
+                    <div className="form-row">
+                        <div className="col form-group">
+                            <label htmlFor="start_time">Start time</label>
+                            <OptDateTimeField name="start_time" onInput={newDate => this.handleUpdate('startTime', newDate)} />
+                        </div>
+
+                        <div className="col form-group">
+                            <label htmlFor="duration">Duration</label>
+                            <div className="input-group">
+                                <PositiveIntegerField className="form-control" name="duration"
+                                                      value={this.state.duration} onInput={v => this.handleUpdate('duration', v)}/>
+                                <div className="input-group-append">
+                                    <label className="input-group-text" htmlFor="duration">minutes</label>
+                                </div>
                             </div>
                         </div>
+                    </div>
 
-                        <hr />
-
-                        <div className="text-center">
-                            <button type="submit" className="btn btn-lg btn-primary px-5"
-                                    disabled={!this.isInputNonzero() || (this.state.hadSubmitAttempt && !this.isValid())}>
-                                Submit
-                            </button>
+                    <div className="form-group form-row">
+                        <label className="col-2 col-form-label" htmlFor="target_url">Subtotal</label>
+                        <div className="col-10 col-form-label">
+                            {
+                                this.isInputNonzero() ? <>
+                                    <span className="text-muted">
+                                        <strong>{this.props.baseCost}</strong>
+                                        {(this.state.numNa !== 0) && <>
+                                            &nbsp;+&nbsp;
+                                            <strong>{this.state.numNa}</strong>&thinsp;&times;&thinsp;<strong>{this.state.duration}</strong>&thinsp;&times;&thinsp;<strong>{this.props.costNa}</strong>
+                                        </>}
+                                        {(this.state.numEu !== 0) && <>
+                                            &nbsp;+&nbsp;
+                                            <strong>{this.state.numEu}</strong>&thinsp;&times;&thinsp;<strong>{this.state.duration}</strong>&thinsp;&times;&thinsp;<strong>{this.props.costEu}</strong>
+                                        </>}
+                                        {(this.state.numA !== 0) && <>
+                                            &nbsp;+&nbsp;
+                                            <strong>{this.state.numA}</strong>&thinsp;&times;&thinsp;<strong>{this.state.duration}</strong>&thinsp;&times;&thinsp;<strong>{this.props.costA}</strong>
+                                        </>}
+                                        &nbsp;=&nbsp;
+                                    </span>
+                                    <strong>{
+                                        (
+                                            this.props.baseCost
+                                                + (this.state.numNa * this.state.duration * this.props.costNa)
+                                                + (this.state.numEu * this.state.duration * this.props.costEu)
+                                                + (this.state.numA * this.state.duration * this.props.costA)
+                                        ).toFixed(2)
+                                    }</strong> ₽
+                                </> : <span className="text-muted">—</span>
+                            }
                         </div>
-                    </form>
-                </div> {/* .card */}
-            </div>
-        );
+                    </div>
+
+                    <hr />
+
+                    <div className="text-center">
+                        <button type="submit" className="btn btn-lg btn-primary px-5"
+                                disabled={!this.isInputNonzero() || (this.state.hadSubmitAttempt && !this.isValid())}>
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div> {/* .card */}
+        </div>;
     }
 }
 
